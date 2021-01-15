6943b31c0d629e76f61a106b87f2cd1b958370d9
anzharip <10259593+anzharip@users.noreply.github.com>
2021-01-15 17:17:03 +0700

    max-subset-sum-no-adjacent

---
+++ src/max-subset-sum-no-adjacent-3/program.ts
@@ -0,0 +1,12 @@
+export function maxSubsetSumNoAdjacent(array: number[]): number {
+  if (array.length === 0) return 0;
+  if (array.length === 1) return array[0];
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+}
---
+++ src/max-subset-sum-no-adjacent-4/test.ts
@@ -0,0 +1,6 @@
+import * as program from './program';
+import * as chai from 'chai';
+
+it('Test Case #1 max-subset-sum-no-adjacent-4', function () {
+  chai.expect(program.maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135])).to.deep.equal(330);
+});
