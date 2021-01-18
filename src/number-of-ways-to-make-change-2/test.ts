bb9aa3d6ed81e1e482775eab02fd595412bf5e82
anzharip <10259593+anzharip@users.noreply.github.com>
2021-01-18 18:06:56 +0700

    number-of-ways-to-make-change

---
+++ src/number-of-ways-to-make-change-2/test.ts
@@ -0,0 +1,7 @@
+import * as program from './program';
+import * as chai from 'chai';
+
+it('Test Case #1 number-of-ways-to-make-change-2', function () {
+  chai.expect(program.numberOfWaysToMakeChange(6, [1, 5])).to.deep.equal(2);
+});
+
