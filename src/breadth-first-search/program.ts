0f76f673f030fc3a8d1dfc9efa858bafa434ed83
anzharip <10259593+anzharip@users.noreply.github.com>
2021-02-15 09:05:43 +0700

    fix: add ! self-assertion

--- src/breadth-first-search/program.ts
+++ src/breadth-first-search/program.ts
@@ -20,7 +20,7 @@ export class Node {
     // Write your code here.
     const queue: Node[] = [this];
     while (queue.length > 0) {
-      const current = queue.shift();
+      const current = queue.shift()!;
       array.push(current.name);
       for (const child of current.children) {
         queue.push(child);
